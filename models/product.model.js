dfb6f30ac7dd546866d432bf95241777401c435b
Nana Kwame <115645394+iamNanaKwame@users.noreply.github.com>
2022-12-18 11:44:24 +0000

    Update product.model.js
    added some module elements

--- models/product.model.js
+++ models/product.model.js
@@ -1,1 +1,29 @@
 //created file
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		sku: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		quantity: {
+			type: Number,
+			default: 0,
+		},
+		createdBy: {
+			type: String,
+			required: true,
+		},
+		createdAt: {
+			type: Date,
+			required: true,
+		},
+	});
+
+module.exports = mongoose.model('product', ProductSchema);
