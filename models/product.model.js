a3d265b514f3ca47d828fa01f1ca5e6578cbcb90
George Yeboah <george.k.yeboah@gmail.com>
2022-12-18 09:23:48 +0000

    included the models for use by all

--- models/product.model.js
+++ models/product.model.js
@@ -1,1 +1,34 @@
 //created file
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		sku: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		quantity: {
+			type: Number,
+			default: 0,
+		},
+		createdBy: {
+			type: String,
+			required: true,
+		},
+		createdAt: {
+			type: Date,
+			required: true,
+		},
+		path: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+	});
+
+module.exports = mongoose.model('product', ProductSchema);
